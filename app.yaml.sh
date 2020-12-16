f156a77d42dea129d295bc2009b60eb3ac8a420d
German Dominguez <gdominguezmontes@gmail.com>
2020-12-16 15:51:30 -0600

    CircleCI Config added and reconfigured

--- app.yaml.sh
+++ app.yaml.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+echo """
 runtime: python39
 entrypoint: gunicorn -b :$PORT main:app
 env_variables:
-  GOOGLE_PROJECT_ID: 'titanic-pdr'
+  GOOGLE_PROJECT_ID: \"$GOOGLE_PROJECT_ID"
+"""
