5532cabf95058f25cb965d2850f24ebf2807bd72
German Dominguez <gdominguezmontes@gmail.com>
2020-12-16 18:08:44 -0600

    CircleCI Config updated configuration

--- app.yaml.sh
+++ app.yaml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo """
 runtime: python39
-entrypoint: gunicorn -b :$PORT main:app
+entrypoint: gunicorn -b :\$PORT main:app
 env_variables:
   GOOGLE_PROJECT_ID: \"$GOOGLE_PROJECT_ID\"
 """
